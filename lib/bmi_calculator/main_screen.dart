702c5674a4ea0198feb79c44c7471e5fbbb64480
slee3593 <slee3593@gatech.edu>
2023-12-21 16:30:10 +0900

    add: 앱 종료시에도 입력값 저장

--- lib/bmi_calculator/main_screen.dart
+++ lib/bmi_calculator/main_screen.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/bmi_calculator/result_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -9,21 +11,62 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   final _formKey = GlobalKey<FormState>();
+  final _heightController = TextEditingController();
+  final _weightController = TextEditingController();
+
+  // 화면이 처음 시작될 때 실행되는 부분
+ @override
+  void initState() {
+    super.initState();
+    load();
+  }
+
+  // 화면 종료시 실행되는 부분
+  // controller 사용시 메모리 효율을 위해 무조건 dispose 사용해야 함
+  @override
+  void dispose() {
+    _heightController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  // 입력값 저장
+  Future save() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setDouble('height', double.parse(_heightController.text),);
+    await prefs.setDouble('weight', double.parse(_weightController.text),);
+  }
+
+  // save에 저장된 값을 받아옴
+  Future load() async {
+    final prefs = await SharedPreferences.getInstance();
+    final double? height = prefs.getDouble('height');
+    final double? weight = prefs.getDouble('weight');
+
+    if (height != null && weight != null) {
+      _heightController.text = '$height';
+      _weightController.text = '$weight';
+    }
+
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        elevation: 2,
+        shadowColor: Colors.black,
         title: const Text('비만도 계산기'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(8.0),
+        padding: const EdgeInsets.all(10.0),
         child: Form(
             key: _formKey,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 TextFormField(
+                  controller: _heightController,
                   decoration: const InputDecoration(
                       // textField의 경계에 선 그려줌
                       border: OutlineInputBorder(),
@@ -33,7 +76,7 @@ class _MainScreenState extends State<MainScreen> {
                     if (value == null || value.isEmpty) {
                       return '키를 입력하세요';
                     } else {
-                      return value;
+                      return null;
                     }
                   },
                 ),
@@ -41,6 +84,7 @@ class _MainScreenState extends State<MainScreen> {
                   height: 8,
                 ),
                 TextFormField(
+                  controller: _weightController,
                   decoration: const InputDecoration(
                       border: OutlineInputBorder(), hintText: '몸무게'),
                   keyboardType: TextInputType.number,
@@ -48,7 +92,7 @@ class _MainScreenState extends State<MainScreen> {
                     if (value == null || value.isEmpty) {
                       return '몸무게를 입력하세요';
                     } else {
-                      return value;
+                      return null;
                     }
                   },
                 ),
@@ -57,9 +101,21 @@ class _MainScreenState extends State<MainScreen> {
                 ),
                 ElevatedButton(
                   onPressed: () {
-                    if (_formKey.currentState?.validate() ?? false) {
-
+                    // form에 올바른 값이 입력되지 않으면 다음 화면으로 넘어가지 않음
+                    if (_formKey.currentState?.validate() == false) {
+                      return;
                     }
+                    save();  // Future - 오래걸림
+
+                    // 다음 화면으로 넘어감
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => ResultScreen(
+                            height: double.parse(_heightController.text),
+                            weight: double.parse(_weightController.text)),
+                      ),
+                    );
                   },
                   child: const Text('결과'),
                 ),
